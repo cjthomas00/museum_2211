5f88fad00badc98172e761c4f985dcd4fc04b362
Caleb Thomas <cjthomas00@comcast.net>
2023-01-03 10:02:49 -0700

    Complete #patrons test and add patrons array to initialize.

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe Museum do
   let(:dead_sea_scrolls) { Exhibit.new({name: "Dead Sea Scrolls", cost: 10}) }
   let(:imax) { Exhibit.new({name: "IMAX",cost: 15}) }
   let(:patron_1) { Patron.new("Bob", 20) }
-  let(:patron_2) { Patron.new("Sally", 20)}
+  let(:patron_2) { Patron.new("Sally", 20) }
+  let(:patron_3) { Patron.new("Johnny", 5) }
 
   describe "#initialize" do
     it "exists" do 
@@ -42,7 +43,18 @@ RSpec.describe Museum do
     end
   end
 
+
   describe "#patrons" do
+    before do 
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2.add_interest("Dead Sea Scrolls")
+      patron_3.add_interest("Dead Sea Scrolls")
+    end
+    
     it "can hold patrons" do 
       expect(dmns.patrons).to eq([])
     end
