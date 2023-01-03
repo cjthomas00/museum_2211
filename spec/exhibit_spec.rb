66ed414851907fd81b4bcad968f6fd8719f13555
Caleb Thomas <cjthomas00@comcast.net>
2023-01-03 09:10:30 -0700

    Write attributes test.

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -5,10 +5,15 @@ RSpec.describe Exhibit do
   let(:exhibit) do 
     Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
-  
+
   describe '#initialize' do
     it "exists" do 
       expect(exhibit).to be_instance_of(Exhibit)
+    end
+
+    it "has attributes" do 
+      expect(exhibit.name).to eq("Gems and Minerals")
+      expect(exhibit.cost).to eq(0)
     end
   end
 end
